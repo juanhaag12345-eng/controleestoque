2799df1a69340cb37e83519f31a4be43cdede638
juanhaag12345-eng <juanhaag12345@gmail.com>
2026-08-16 18:03:37 -0300

    Aumenta limite do body parser para suportar imagens de ate 10MB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-app.use(express.json({ limit: "15mb" }));
+app.use(express.json({ limit: "20mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const DATA_FILE = path.join(__dirname, "data.json");
